e5629b742ef0ca3ed816ae9ced27483301cf8e5c
Gerardo Castillo <gerrcass@gmail.com>
2020-07-19 22:15:40 -0400

    extracting container component (VisibleTodoList/AddTodo). This way only specific container components subscribe to redux store instead of the main TodoApp

--- todoApp.js
+++ todoApp.js
@@ -120,14 +120,18 @@ const TodoList = ({ todos, onTodoClick }) => (
   </ul>
 );
 
-const AddTodo = ({ onAddClick }) => {
+const AddTodo = () => {
   let input;
   return (
     <>
       <input ref={(node) => (input = node)} />
       <button
         onClick={() => {
-          onAddClick(input.value);
+          store.dispatch({
+            type: "ADD_TODO",
+            id: nextTodoId++,
+            text: input.value,
+          });
           input.value = "";
         }}
       >
@@ -147,32 +151,31 @@ const getVisibleTodos = (todos, filter) => {
   }
 };
 
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    this.unsubdcribe = store.subscribe(() => this.forceUpdate());
+  }
+  componenWillUnmount() {
+    this.unsubdcribe();
+  }
+  render() {
+    const { todos, visibilityFilter } = store.getState();
+    return (
+      <TodoList
+        todos={getVisibleTodos(todos, visibilityFilter)}
+        onTodoClick={(id) => store.dispatch({ type: "TOGGLE_TODO", id })}
+      />
+    );
+  }
+}
+
 let nextTodoId = 0;
-const TodoApp = ({ todos, visibilityFilter }) => (
+const TodoApp = () => (
   <>
-    <AddTodo
-      onAddClick={(text) =>
-        store.dispatch({
-          type: "ADD_TODO",
-          id: nextTodoId++,
-          text,
-        })
-      }
-    />
-    <TodoList
-      todos={getVisibleTodos(todos, visibilityFilter)}
-      onTodoClick={(id) => store.dispatch({ type: "TOGGLE_TODO", id })}
-    />
+    <AddTodo />
+    <VisibleTodoList />
     <Footer />
   </>
 );
 
-const render = () => {
-  ReactDOM.render(
-    <TodoApp {...store.getState()} />,
-    document.getElementById("root")
-  );
-};
-
-store.subscribe(render);
-render();
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
