ba825f2eaa1f24a20301fca2663c1a10db2d926e
Gerardo Castillo <gerrcass@gmail.com>
2020-07-24 10:49:21 -0400

    Normalizing the state shape of the todos. The state redux shaped is now more like a database which can be sync between various data structures to get enough flexibility.

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,43 +1,46 @@
-const todo = (state, action) => {
-  switch (action.type) {
-    case "ADD_TODO":
-      return { id: action.id, text: action.text, completed: false };
-    case "TOGGLE_TODO":
-      if (state.id !== action.id) {
-        return state;
-      }
-      return { ...state, completed: !state.completed };
-    default:
-      return state;
-  }
-};
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case "ADD_TODO":
-      return [...state, todo(undefined, action)];
-    case "TOGGLE_TODO":
-      return state.map((t) => todo(t, action));
+import { combineReducers } from "redux";
+import todo from "./todo";
 
-    /* // The same as above but using conditional (ternary) operator
-          return state.map((todo) =>
-            todo.id !== action.id ? todo : { ...todo, completed: !todo.completed }
-          ); */
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        [action.id]: todo([action.id], action),
+      };
     default:
       return state;
   }
 };
+
+const allIds = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, action.id];
+    default:
+      return state;
+  }
+};
+
+const todos = combineReducers(byId, allIds);
 
 export default todos;
-
 // The default export is always the reducer function, but any named export starting with get is a so called SELECTOR,
 // that is to say a function that prepares the data to be displayed by the UI.
+
+const getAllTodos = (state) => state.allIds.map((id) => state.byId[id]);
+
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state);
   switch (filter) {
     case "all":
-      return state;
+      return allTodos;
     case "completed":
-      return state.filter((todo) => todo.completed);
+      return allTodos.filter((todo) => todo.completed);
     case "active":
-      return state.filter((todo) => !todo.completed);
+      return allTodos.filter((todo) => !todo.completed);
+    default:
+      throw new Error(`Unkown filter ${filter}.`);
   }
 };
