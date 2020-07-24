d8bb5e5859f0d8f284445a918fb81028d52f6602
Gerardo Castillo <gerrcass@gmail.com>
2020-07-24 06:44:54 -0400

    mapDispatchToProps shorthand: when the arguments passed through the callback props injected to the

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -23,12 +23,17 @@ const mapStateToProps = (
 ) => ({
   todos: getVisibleTodos(state.todos, filter || "all"),
 });
-const mapDispatchToProps = (dispatch) => ({
+/* const mapDispatchToProps = (dispatch) => ({
   onTodoClick(id) {
     dispatch(toggleTodo(id));
   },
   //onTodoClick: (id) => dispatch(toggleTodo(id)),
-});
+}); */
+
+/* mapDispatchToProps shorthand: when the arguments passed through the callback props injected to the
+component are passed through to the action creators in the same order, it can be passed a configuration
+object so that, under the hood, dispatch() maps the names of the callback props to the corresponding
+action creator function. */
 export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(TodoList)
+  connect(mapStateToProps, { onTodoClick: toggleTodo })(TodoList)
 );
